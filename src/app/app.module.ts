ad4c54657ca1f24f96a27ad650b76a7f10c28e9b
Federico <fluque87@gmail.com>
2024-10-19 15:35:11 +0200

    Modificacion del componente clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { RegistroComponent } from './componentes/registro/registro.component';
 import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
 import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
 import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
+import { ClienteServicio } from './servicios/cliente.service';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,7 @@ import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.componen
     AngularFireAuthModule,  // Proveedor de Auth
     AngularFireStorageModule // Proveedor de Storage
   ],
-  providers: [],
+  providers: [ClienteServicio],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ClienteServicio } from '../../servicios/cliente.service';
+import { Cliente } from '../../modelo/cliente.model';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
-  styleUrl: './clientes.component.css'
+  styleUrl: './clientes.component.css',
 })
 export class ClientesComponent {
+  clientes: Cliente[];
 
+  constructor(private clienteServicio: ClienteServicio) {}
+
+  ngOnInit() {
+    this.clienteServicio.getClientes().subscribe((clientes) => {
+      this.clientes = clientes;
+    });
+  }
 }
