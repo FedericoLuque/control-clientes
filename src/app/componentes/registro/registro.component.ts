ee583cf5a3f9d76cb3183afc4dcafdf6b990d5ea
Federico <fluque87@gmail.com>
2024-11-08 22:23:47 +0100

    Añadida la funcionalidad de registro

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertMessagesService } from 'jjwins-angular-alert-messages';
+import { LoginService } from '../../servicios/login.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class RegistroComponent {
 
+  email: string;
+  password: string;
+
+  constructor(
+    private router: Router,
+    private alertMessageService: AlertMessagesService,
+    private loginService: LoginService
+  ) {}
+
+  ngOnInit(){
+    this.loginService.getAuth().subscribe(auth =>{
+      if(auth){
+        this.router.navigate(['/']);
+      }
+    })
+  }
+
+  registro(){
+    this.loginService.registrarse(this.email, this.password)
+    .then( res => {
+      this.router.navigate(['/'])
+    })
+    .catch(error => {
+      this.alertMessageService.show( error.message, {
+        cssClass: 'alerts-error', timeout: 4000
+      } )
+    })
+  }
 }
